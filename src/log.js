44d9625fc74fc8c87e16e2c2a180399e5d1162de
DanSketic <dansketic@gmail.com>
2023-07-17 11:19:52 +0200

    Update log.js

--- src/log.js
+++ src/log.js
@@ -2,13 +2,13 @@ const chalk = require('chalk')
 
 const log = {
 	info(text){
-		console.log(chalk.bold.bgBlue.rgb(255,255,255)(`\n [gvsdk] info -> `), chalk.underline(`${text} \n`))
+		console.log(chalk.bold.bgBlue.rgb(255,255,255)(`\n [fmsdk] info -> `), chalk.underline(`${text} \n`))
 	},
 	success(text){
-		console.log(chalk.bold.bgGreen(`\n [gvsdk] -> `), chalk.white(`${text} \n`))
+		console.log(chalk.bold.bgGreen(`\n [fmsdk] -> `), chalk.white(`${text} \n`))
 	},
 	error(text){
-		console.log(chalk.bold.bgRed(`\n [gvsdk] error -> `), chalk.white(`\n\n ${text} \n`))
+		console.log(chalk.bold.bgRed(`\n [fmsdk] error -> `), chalk.white(`\n\n ${text} \n`))
 	}
 }
 
